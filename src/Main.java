176cc836596067adac9c79a067235c0c6cd37447
Robert99256 <robertgabriel.popoviciu@ulbsibiu.ro>
2025-03-31 23:14:12 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String file = "src/persoane.txt";
+        List<Persoana> listaPersoane = citireFisier(file);
+
+        if (listaPersoane.isEmpty()) {
+            System.out.println("Nu s-au gasit persoane in fisier.");
+            return;
+        }
+
+        listaPersoane.sort(Comparator.comparing(Persoana::getNume));
+
+        afisareEcran(listaPersoane);
+    }
+
+    public static List<Persoana> citireFisier(String file) {
+        List<Persoana> listaPersoane = new ArrayList<>();
+        try (Scanner sc = new Scanner(new File(file))) {
+            while (sc.hasNextLine()) {
+                String linie = sc.nextLine().trim();
+                if (!linie.isEmpty()) {
+                    String[] date = linie.split("\\s+");
+                    if (date.length == 3) {
+                        String nume = date[0].trim();
+                        String prenume = date[1].trim();
+                        int varsta = Integer.parseInt(date[2].trim());
+                        listaPersoane.add(new Persoana(nume, prenume, varsta));
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Fisierul nu a fost gasit");
+        } catch (NumberFormatException e) {
+            System.out.println("Eroare la citirea varstei din fisier.");
+        }
+        return listaPersoane;
+    }
+
+    public static void afisareEcran(List<Persoana> listaPersoane) {
+        for (Persoana p : listaPersoane) {
+            System.out.println(p);
+        }
+    }
+}
---
+++ src/Persoana.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+class Persoana {
+    private String nume;
+    private String prenume;
+    private int varsta;
+
+    public Persoana(String nume, String prenume, int varsta) {
+        this.nume = nume;
+        this.prenume = prenume;
+        this.varsta = varsta;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public int getVarsta() {
+        return varsta;
+    }
+
+    @Override
+    public String toString() {
+        return nume + " " + prenume + "  " + varsta + " ani";
+    }
+}
